513fb19fcfde647f43dac9e4d4d225d03a58d8da
aaronfeingold <ajfeingold88@gmail.com>
2021-05-21 03:35:01 -0500

    updated readme

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('has a submit button', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/Submit/i);
-  expect(linkElement).toBeInTheDocument();
-});

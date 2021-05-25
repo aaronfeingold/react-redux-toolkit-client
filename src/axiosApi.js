30cd5f0db55443a1502af625879b796d82ded668
aaronfeingold <ajfeingold88@gmail.com>
2021-05-25 02:11:20 -0500

    production

--- src/axiosApi.js
+++ src/axiosApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "https://express-validator-proj.herokuapp.com/api",
   headers: {
     "Content-type": "application/json"
   }
